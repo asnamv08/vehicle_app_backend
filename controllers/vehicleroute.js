9615cce8f7ce875a3f441eb9d153010ad146b901
asnamv08 <asnamvarghese001@gmail.com>
2024-02-09 03:26:02 +0530

    added

--- controllers/vehicleroute.js
+++ controllers/vehicleroute.js
@@ -14,6 +14,10 @@ router.get("/view",async(req,res)=>{
     let data=await vehiclemodel.find()
     res.json(data)
 })
-
+router.post("/search",async(req,res)=>{
+    let input=req.body
+    let data=await vehiclemodel.find(input)
+    res.json(data)
+})
 
 module.exports=router
